85dbd75a6f5c77cd69076bdede21e6725d571598
smjcrane <sc989@cam.ac.uk>
2017-11-16 20:03:19 +0000

    can now see scores

--- game9.py
+++ game9.py
@@ -15,8 +15,7 @@ def play_game(player1, player2):
         progress = not board.is_full()
         player2.play(board, 2)
         GUI.update(board)
-        progress = not board.is_full()
-        
+        progress = not board.is_full()   
     return board.get_scores()
         
 
